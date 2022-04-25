37ff0fa8994f77034e917064d18f472a3c9b00e7
kopylovvlad <kopylov.vlad@gmail.com>
2022-04-25 16:43:35 +0300

    Add Api::Group test

--- todo_list/spec/requests/api/groups/show_spec.rb
+++ todo_list/spec/requests/api/groups/show_spec.rb
@@ -22,5 +22,26 @@ MiniRSpec.describe 'Api::Group' do
         expect(json['id']).to eq(groups.first.id)
       end
     end
+
+    context 'attributes' do
+      let!(:group) { create(:group) }
+      let!(:active_items) { create(:item, group_id: group.id) }
+      let!(:not_active_items) { create_list(:item, 2, :done, group_id: group.id) }
+
+      before_each { get "/api/groups/#{group.id}" }
+
+      it 'renders full title' do
+        full_title = "#{group.title}: #{group.description}"
+        expect(json['full_title']).to eq(full_title)
+      end
+
+      it 'renders active count' do
+        expect(json['active_count']).to eq(1)
+      end
+
+      it 'renders not active count' do
+        expect(json['not_active_count']).to eq(2)
+      end
+    end
   end
 end
